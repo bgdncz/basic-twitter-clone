546e59b9929ec85909bd66ec9b9baf5b41aed607
Bogdan Cuza <bogdancuza@protonmail.com>
2022-06-29 22:26:04 +0300

    Defining followers and followeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_many :tweets
+    has_many :active_relationships, class_name: "Relationship", foreign_key: :follower_id
+    has_many :passive_relationships, class_name: "Relationship", foreign_key: :followed_id
+    has_many :followeds, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
 end
