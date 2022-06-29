d350ab3b2f2043b86e7c6d652effc344d0801c28
Bogdan Cuza <bogdancuza@protonmail.com>
2022-06-29 22:28:44 +0300

    Select from list of user names instead of ids

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
     has_many :passive_relationships, class_name: "Relationship", foreign_key: :followed_id
     has_many :followeds, through: :active_relationships, source: :followed
     has_many :followers, through: :passive_relationships, source: :follower
+
+    def to_s
+        "#{self.name} (@#{self.username})"
+    end
 end
